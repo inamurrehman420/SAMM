153d7c82eb5ad80622fbff33140a194983585f4e
unknown <abdulsamirana27@gmail.com>
2023-10-27 07:26:30 -0700

    changes

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { RecipeService } from '../../appointments/recipe.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { finalize } from 'rxjs';
 
 interface Report {
   value: string;
@@ -22,11 +28,40 @@ export class DashboardComponent {
     {value: '2', viewValue: 'Appointment reports'},
     {value: '3', viewValue: 'Operational report'},
   ];
-
-  constructor(private dialog: MatDialog, private dialogRef: MatDialog,){}
+Recipes=[];
+  constructor(private dialog: MatDialog,
+              private dialogRef: MatDialog,
+              private recipeService:RecipeService,
+              private toastr: ToastrService,
+              private spinner: NgxSpinnerService,
+              private _router:Router){}
     ngOnInit() {
+      this.GetRecipe();
     }
 
-   
+    
+
+    GetRecipe(){
+      this.spinner.show();
+      this.recipeService.GetRecipe({   "page":1,
+      "limit":3})
+      .pipe(
+          finalize(() => {
+            this.spinner.hide();
+          })
+      ).subscribe((res) => {
+          if (res.success === true) {
+            this.Recipes =res.data;
+            // this.dataSource.paginator = this.paginator;
+            // this.dataSource.sort = this.sort;
+            // this.dataSource.paginator.length = res.data.total_records;
+            // this.length = res.data[0].total_records;
+            // this.toastr.success('Login Successfully','Success');
+          } else { 
+            this.toastr.error('Something went wrong','Failed');
+             
+          }
+      });
+    }
 
 }
